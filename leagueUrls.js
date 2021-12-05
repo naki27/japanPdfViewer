4bae65731a50c1a35cef5e791830d5de4b0f16c8
naki27 <kyo.jin.go@gmail.com>
2021-12-05 10:21:59 +0900

    add kyushu tournament

--- leagueUrls.js
+++ leagueUrls.js
@@ -11,18 +11,31 @@ const leagueUrls = _ => {
     // 女子50 https://drive.google.com/file/d/1Kyj0XC1FJJ1LOTLyuZlQG1s9vo65UcfI/view?usp=sharing
     // 女子60 https://drive.google.com/file/d/1WICfdClaEC5TxWTjKcbafNYbIZLcsHqh/view?usp=sharing
 
-    return [
-        "https://drive.google.com/uc?id=1XwHSeR0bKYIwGTegov_8me7S4li4OqJF&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1tNVXsUtRCYdwBD1FQCzU3CXWBnl9jqIY&export=download&format=pdf",
-        "https://drive.google.com/uc?id=10kDNS_g4ihcIRAp1KgGQ8igFRK3h7AKw&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1-wVo3B1u6_cc593Pq3-DcdYssVNFIpQy&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1LdkfaYWkVk3hPjDrXykMT2P6udXWzsw5&export=download&format=pdf",
+    // ジャパン
+    // return [
+    //     "https://drive.google.com/uc?id=1XwHSeR0bKYIwGTegov_8me7S4li4OqJF&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1tNVXsUtRCYdwBD1FQCzU3CXWBnl9jqIY&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=10kDNS_g4ihcIRAp1KgGQ8igFRK3h7AKw&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1-wVo3B1u6_cc593Pq3-DcdYssVNFIpQy&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1LdkfaYWkVk3hPjDrXykMT2P6udXWzsw5&export=download&format=pdf",
 
-        "https://drive.google.com/uc?id=1sE8gPKL8zIbtc1iI_FGFh8QiIxcJwyib&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1t9ok6YBazvFOFoF3IYkLq9H4GcLbWvRL&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1LR_mA2J_4pHReG6olFjSdkiAEp81ttfJ&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1Kyj0XC1FJJ1LOTLyuZlQG1s9vo65UcfI&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1WICfdClaEC5TxWTjKcbafNYbIZLcsHqh&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1sE8gPKL8zIbtc1iI_FGFh8QiIxcJwyib&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1t9ok6YBazvFOFoF3IYkLq9H4GcLbWvRL&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1LR_mA2J_4pHReG6olFjSdkiAEp81ttfJ&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1Kyj0XC1FJJ1LOTLyuZlQG1s9vo65UcfI&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1WICfdClaEC5TxWTjKcbafNYbIZLcsHqh&export=download&format=pdf",
+    // ];
+
+    //九州大会
+    // 男子ALL https://drive.google.com/file/d/16I3BxzocDzc6ZKw4kuYAqWmZyrSLfgG2/view?usp=sharing
+    // 女子18 https://drive.google.com/file/d/16I3BxzocDzc6ZKw4kuYAqWmZyrSLfgG2/view?usp=sharing
+    // 女子30 https://drive.google.com/file/d/1-CR3FaZ96xRpEebdwZ3xuKrUoCbCsOgt/view?usp=sharing
+    // 女子40 https://drive.google.com/file/d/1b-dY45q1Qjz08S5oEzAu_-7PBLiywrie/view?usp=sharing
+    return [
+        "https://drive.google.com/uc?id=16I3BxzocDzc6ZKw4kuYAqWmZyrSLfgG2&export=download&format=pdf",
+        "https://drive.google.com/uc?id=16I3BxzocDzc6ZKw4kuYAqWmZyrSLfgG2&export=download&format=pdf",
+        "https://drive.google.com/uc?id=1-CR3FaZ96xRpEebdwZ3xuKrUoCbCsOgt&export=download&format=pdf",
+        "https://drive.google.com/uc?id=1b-dY45q1Qjz08S5oEzAu_-7PBLiywrie&export=download&format=pdf"
     ];
 }
 
--- tournamentUrls.js
+++ tournamentUrls.js
@@ -11,18 +11,37 @@ const tournamentUrls = _ => {
     // 女子50 https://drive.google.com/file/d/1bVj4Up2zJbvwB31Bkurd3Er2a77jqBEb/view?usp=sharing
     // 女子60 https://drive.google.com/file/d/1l8G5xehFR0SAt3IJFc038b22ElY4KQyL/view?usp=sharing
 
-    return [
-        "https://drive.google.com/uc?id=1I13bu_qm1keuk4gOxwyc8eRf-ZTc6EZy&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1Ap2o3WHhIHZTc3EHncgshf0JymLaoeFp&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1ZuGC-jX9uO95BQHgzC6waFkrgaKYpQMj&export=download&format=pdf",
-        "https://drive.google.com/uc?id=10b8lYw7wbRI92y4x08ysgHeoazjU9tHR&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1tn01cKQsuEu4h2xFFYEQP1Eu3LyYV46Q&export=download&format=pdf",
+    //japan
+    // return [
+    //     "https://drive.google.com/uc?id=1I13bu_qm1keuk4gOxwyc8eRf-ZTc6EZy&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1Ap2o3WHhIHZTc3EHncgshf0JymLaoeFp&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1ZuGC-jX9uO95BQHgzC6waFkrgaKYpQMj&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=10b8lYw7wbRI92y4x08ysgHeoazjU9tHR&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1tn01cKQsuEu4h2xFFYEQP1Eu3LyYV46Q&export=download&format=pdf",
 
-        "https://drive.google.com/uc?id=1al9sWZB9v80U7bnyr6PV9hNzR4yYLvZ6&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1eQ78T6AZE7HK98bsRSG69N1tVpC9S0sp&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1YomQcY5tStZwITWVi5zSUKGfossJglKu&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1bVj4Up2zJbvwB31Bkurd3Er2a77jqBEb&export=download&format=pdf",
-        "https://drive.google.com/uc?id=1l8G5xehFR0SAt3IJFc038b22ElY4KQyL&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1al9sWZB9v80U7bnyr6PV9hNzR4yYLvZ6&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1eQ78T6AZE7HK98bsRSG69N1tVpC9S0sp&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1YomQcY5tStZwITWVi5zSUKGfossJglKu&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1bVj4Up2zJbvwB31Bkurd3Er2a77jqBEb&export=download&format=pdf",
+    //     "https://drive.google.com/uc?id=1l8G5xehFR0SAt3IJFc038b22ElY4KQyL&export=download&format=pdf",
+    // ];
+
+    //九州大会
+    // 男子ALL↑ https://drive.google.com/file/d/1fBiWFHZXXl9eOpN_-0GYfFyhZ4KZLh7a/view?usp=sharing
+    // 男子ALL↓ https://drive.google.com/file/d/19w2BhJaV94Nd9cALFMoCVUNAmlzYyiDK/view?usp=sharing
+
+    // 女子18 https://drive.google.com/file/d/1G6WXHAwPKhMORn6mkdVW0QDcanhddTa_/view?usp=sharing
+    // 女子30 https://drive.google.com/file/d/1DxT7nKxBijymij4c6PNqwgXoM8eEf-fV/view?usp=sharing
+    // 女子40↑ https://drive.google.com/file/d/1xjJO2JJL4G5jf7_VGQW-x0ygYGoNl9rg/view?usp=sharing
+    // 女子40↓ https://drive.google.com/file/d/1v1aCoeAn2eZf__S6JUfbx79QPlFJrJBB/view?usp=sharing
+    return[
+        "https://drive.google.com/uc?id=1fBiWFHZXXl9eOpN_-0GYfFyhZ4KZLh7a&export=download&format=pdf",
+        "https://drive.google.com/uc?id=19w2BhJaV94Nd9cALFMoCVUNAmlzYyiDK&export=download&format=pdf",
+
+        "https://drive.google.com/uc?id=1G6WXHAwPKhMORn6mkdVW0QDcanhddTa_&export=download&format=pdf",
+        "https://drive.google.com/uc?id=1DxT7nKxBijymij4c6PNqwgXoM8eEf-fV&export=download&format=pdf",
+        "https://drive.google.com/uc?id=1xjJO2JJL4G5jf7_VGQW-x0ygYGoNl9rg&export=download&format=pdf",
+        "https://drive.google.com/uc?id=1v1aCoeAn2eZf__S6JUfbx79QPlFJrJBB&export=download&format=pdf",
     ];
 }
 
